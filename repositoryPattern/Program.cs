d4b76ed9867eaeb539bcc1ede5297cbb6cd1547f
PC <PC@DESKTOP-613>
2017-08-03 13:19:03 +0200

    Actualización

--- repositoryPattern/Program.cs
+++ repositoryPattern/Program.cs
@@ -15,16 +15,49 @@ namespace repositoryPattern
                 1, 2, 3
             };
             var a = 10;
-            var result = a.Between(9,20);// Between aparece con una flecha abajo por ser una extensión. Where también lo es, implementada por microsoft.
-            numbers.Where(Predicate); // Se le puede pasar el método predicate ya que where si nos vamos con f12 al origen, es una función genérica que requiere especificar la salida y el parámetro, dado que predicate lo cumple, se puede poner como parámetro para utilizarlo.
-            // De aquí viene --> public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate); Se especifica que Tsource será el parámetro de entrada y el utilizado, de ahí que no se pden usar tipos de datos diferentes en el parámetro y luego en el método.
-            numbers.Where(n => n > 1); // Esto es una lambda expression, que no tienen return. n sería value | => es un método anónimo, el que sea, y le decimos que devolverá si n>1. es una función con una entrada, devuelve un booleano, de forma que coincide con el where y puede usarse.
-            // Sería equivalente a esta función:    
+            var result = a.Between(9,20);           // Between aparece con una flecha abajo por ser una extensión. Where también lo es, implementada por microsoft.
+            var result2 = numbers.Where(Predicate); // Se le puede pasar el método predicate ya que where si nos vamos con f12 al origen, es una función genérica que requiere especificar la salida y el parámetro, dado que predicate lo cumple, se puede poner como parámetro para utilizarlo.
+                                                    // Este where no se implementa hasta que no se itera por él (al hacer debug sí aparecen los elementos, porque s eitera por ella para poder ver los resultados). Se queda como una intención, pero no se ejecuta. Si se muestra por pantalla, no dará el resultado esperado.
+            Console.WriteLine(result2);             // Devuelve --> System.Linq.Enumerable+WhereListIterator`1[System.Int32]
+                                                    // De aquí viene --> public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate); Se especifica que Tsource será el parámetro de entrada y el utilizado, de ahí que no se pden usar tipos de datos diferentes en el parámetro y luego en el método.
+            numbers.Where(n => n > 1);              // Esto es una lambda expression, que no tienen return. n sería value | => es un método anónimo, el que sea, y le decimos que devolverá si n>1. es una función con una entrada, devuelve un booleano, de forma que coincide con el where y puede usarse. Hasta que no iteras sobre ella no existe el resultado. Si añadiera .ToList() al final, entonces sí que existiría el resultado porque se iteraría sobre ella para generar la lista. Si hay que resolver algo y hacer lógica de negocio, se usa tolist, pero si no, no se hace.
+            // Sería equivalente a la función de la linea 42 predicate.    
+            foreach (var i in Iterator())
+            {
+                Console.WriteLine(i);
+            }
+
+            var resultado = iterar();
+            foreach (var value in resultado) // Al iterar por él, aunque no haya código, ya entra. También serviría con las lambda expressions de la linea 23, ya que al iterar sobre ellas devuelven el resultado.
+            {
+
+            }
+            Console.ReadLine();
+            {
+
+            }
+
         }
         
         static bool Predicate(int value) // Aunque se vaya a usar como un genérico al que le vale todo, al declarar int en el parámetro, limitamos el tipo de datos que acepta el método.
         {
+            Console.WriteLine("he entrado");
             return value > 1;
+        }
+        static IEnumerable<int> Iterator()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                yield return i; // El  yield llama al método predicate. Al devolver un ienumerate, no se devolvería nada hasta iterar las 100 veces, lo que se llama que es bloqueante, pero al poner un yield, hace que el return devuelva el valor que tenga, ya que devuelve el control al que ha invocado al método.
+            }
+        }
+        static bool iterar()
+        {
+            foreach (var item in collection)
+            {
+
+            }
+            ?¿
         }
     }
 
